965fcda778e2444b11e0087c37cd0c74fd104348
Ibad Khan <ik.ibadkhan@gmail.com>
2015-11-03 22:23:54 -0800

    Insert a node at a specific position in a linked list

--- linked_list/list_easy.c
+++ linked_list/list_easy.c
@@ -7,34 +7,67 @@ typedef struct _node
     struct _node* next;
 }node;
 
-// Insert a nodet at the head of a linked list
-node* beginning(node* head, int data)
+// Creates a linked list node
+node* create_node(int data)
 {
     node* n = (node *) malloc(sizeof(node));
     n->data = data;
+    n->next = NULL;
+    return n;
+}
+
+// Insert a node at the head of a linked list
+node* insert_at_head(node* head, int data)
+{
+    node* n = create_node(data);
     n->next = head;
     return n;
 }
 
 // Insert a node at the tail of a linked list
-node* tail(node* head, int data)
+node* insert_at_tail(node* head, int data)
 {
-    node* n = (node *) malloc(sizeof(node));
-    n->data = data;
-    n->next = NULL;
-
+    node* n = create_node(data);
     if (head == NULL)
     {
         return n;
     }
+    node *curr;
+    for (curr = head; curr->next != NULL; curr = curr->next);
+    curr->next = n;
+    return head;
+}
+
+// Insert a node at a specific position in a linked list
+node* insert_at_pos(node* head, int data, int pos)
+{
+    node *n = create_node(data);
+    if (head == NULL)
+    {
+        return n;
+    }
+    else if (pos == 0)
+    {
+        n->next = head;
+        return n;
+    }
     else
     {
-        node *temp = head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = n;
-        return head;
+        int i = 1;
+        node *curr = head;
+        while (curr != NULL)
+        {
+            if (i == pos)
+            {
+                n->next = curr->next;
+                curr->next = n;
+                return head;
+            }
+            i++;
+            curr = curr->next;
+        }
     }
+    return head;
 }
 
 // Print the elements of a linked list
@@ -51,9 +84,11 @@ int main()
 {
     node *head = NULL;
 
-    head = beginning(head, 1);
-    head = beginning(head, 2);
-    head = beginning(head, 3);
+    head = insert_at_pos(head, 1, 0);
+    head = insert_at_pos(head, 2, 1);
+    head = insert_at_pos(head, 3, 2);
+    head = insert_at_pos(head, 4, 1);
+    head = insert_at_pos(head, 5, 1);
 
     display(head);
 }
